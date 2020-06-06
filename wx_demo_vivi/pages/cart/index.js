2de12766b3ba5b971bca9f9822633af056091e72
vivi <819233365@qq.com>
2020-06-06 16:25:12 +0800

    6-6 address

--- wx_demo_vivi/pages/cart/index.js
+++ wx_demo_vivi/pages/cart/index.js
@@ -1,66 +1,43 @@
 // pages/cart/index.js
+/**
+ * 1.获取用户的收货地址
+ *  1.点击绑定事件
+ *  2.调用小程序内置api 获取用户的收货地址(走不通)
+ *  2.获取用户 对小程序 所授予 获取地址的 权限状态 scope
+ *    1.假设 用户 点击获取收货地址的提示框 确定  authSetting scope.address   scope为 true
+ *    2.假设 用户 点击获取收货地址的提示框 取消
+ *      scope为 false
+ *        1.诱导用户自己 打开 授权设置页面 当用户重新 给与 获取地址权限的时候
+ *        2.获取收货地址
+ *    3.假设 用户 从来没有调用过这个收货地址的api
+ *      scope为 undefined
+ *    4.把获取到的收货地址保存到缓存中
+ */   
+import { getSetting, chooseAddress, openSetting } from '../../utils/asyncWx'
+import regeneratorRuntime, { async } from '../../lib/runtime/runtime';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
 
   },
+  //获取收货地址
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    async handleChooseAddress() {
+      try {
+      //1.获取权限状态
+      const res1 = await getSetting();
+      const scopeAdress = res1.authSetting["scope.address"]
+      //2.判断权限状态
+      if (scopeAdress === false) {
+        //3.先诱导用户打开授权页面
+        await openSetting();
+      } 
+        //4.调用获取收货地址的api
+        const address = await chooseAddress();
+        wx.setStorageSync('address', address);
+          
+    }catch(error){
+      console.log(error)
+    }
   }
+ 
 })
--- wx_demo_vivi/pages/goods_detail/index.js
+++ wx_demo_vivi/pages/goods_detail/index.js
@@ -5,6 +5,13 @@
  * 2.点击轮播图 预览大图
  *  1.给轮播图绑定点击事件
  *  2.调用小程序的api previewImage
+ * 3.点击加入购物车
+ *  1.先绑定点击事件
+ *  2.获取缓存中的购物车数据 数组格式
+ *  3.先判断当前的商品是否存在于购物车
+ *  4.已经存在 修改商品数据 执行购物车数量 ++ 重新把购物车数组，填充回缓存中
+ *  5.不存在于购物车的数组中，直接给购物车数组添加一个新元素 新元素带上购买数量属性 num 重新把购物车数组， 填充回缓存中
+ *  6.弹出用户提示
  */
 
 import {request} from '../../request/index.js'
@@ -60,6 +67,35 @@ Page({
     });
       
 
+  },
+  //加入购物车
+  handleAddCart(){
+   //1.获取缓存中的购物车 数组
+   let cart = wx.getStorageSync('cart')||[];
+   //2.判读商品对象是否存在于购物车数组中
+   let index = cart.findIndex(v=>v.goods_id==this.GoodsInfo.goods_id);
+   console.log(index)
+   if(index==-1){
+     //3.不存在，第一次添加
+     this.GoodsInfo.num=1;
+     cart.push(this.GoodsInfo);
+     console.log(cart)
+   }else{
+     //4.已经存在购物车数据。执行 num++
+     cart[index].num ++
+   }
+   //5.把购物车重新添加回缓存中
+   wx.setStorageSync('cart',cart)
+   //6.弹窗提示
+   wx.showToast({
+     title: '加入成功',
+     icon:'success',
+     //true 防止用户手抖 疯狂点击按钮
+     mask: true
+   
+   });
+     
+     
   },
   onReady: function () {
 
